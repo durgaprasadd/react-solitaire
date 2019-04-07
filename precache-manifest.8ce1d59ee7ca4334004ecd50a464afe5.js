be384891282ecbba03e44f77612653f3ce90c404
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-04-07 18:27:00 +0530

    Updates

---
+++ precache-manifest.8ce1d59ee7ca4334004ecd50a464afe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c1a7d75fef5984be1e8",
+    "url": "/react-solitaire/static/js/runtime~main.7ea19e75.js"
+  },
+  {
+    "revision": "8a81a5c6a35abde646ba",
+    "url": "/react-solitaire/static/js/main.50f4da36.chunk.js"
+  },
+  {
+    "revision": "faebcb6edb8639c04f1b",
+    "url": "/react-solitaire/static/js/2.407cf3ba.chunk.js"
+  },
+  {
+    "revision": "8a81a5c6a35abde646ba",
+    "url": "/react-solitaire/static/css/main.0c4ed494.chunk.css"
+  },
+  {
+    "revision": "2588585d2069fcaa431d2fe44337c1b9",
+    "url": "/react-solitaire/index.html"
+  }
+];
